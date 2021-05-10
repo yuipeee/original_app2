e6ea1dc7950df882931c7ea0a72913c31ef8c900
yuipeee <nooof.0827@gmail.com>
2021-05-10 23:33:50 +0900

    検索挙動

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,15 +2,8 @@ class Menu < ApplicationRecord
   belongs_to :food
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :style
+  belongs_to_active_hash :style
   belongs_to :material
   belongs_to :feeling
-
-  #ジャンルの選択が「--」の時は保存できないようにする
-  with_options numericality: { other_than: 1 }  do
-  validates :style_id
-  validates :material_id
-  validates :feeling_id
-  end
 
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,6 +1,5 @@
 class Style < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
     { id: 2, name: '和風' },
     { id: 3, name: '洋風' },
     { id: 4, name: '中華料理' }
